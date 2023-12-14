55dd28cfef0357af56d960bf71c120f802eef306
Mahdi Alethea <aletheamahdi@gmail.com>
2023-12-14 17:37:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihanapp8/scroll_comedy.java
@@ -0,0 +1,15 @@
+package com.example.latihanapp8;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class scroll_comedy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_1);
+    }
+
+}
